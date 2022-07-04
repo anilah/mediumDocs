1f164dda55c3a8efdfc9cf429fe2a7338d2b33bf
Sangeeta Kadyan <>
2022-07-04 17:20:05 +1200

    Create Untitled (1).py

---
+++ Untitled (1).py
@@ -0,0 +1,26 @@
+%region ap-southeast-2
+
+%profile default
+
+%iam_role arn:aws:iam::*********:role/glueServiceRole
+
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+glueContext = GlueContext(SparkContext.getOrCreate())
+
+test_dynamicframe = glueContext.create_dynamic_frame.from_options(
+    's3',
+    {'paths': ['s3://bt-test-bck/input']},
+    'csv',
+    {'withHeader': True})
+print("Count:",test_dynamicframe.count())
+test_dynamicframe.printSchema()
+
+%stop_session
+
+
